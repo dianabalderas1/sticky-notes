84557f7f6859c84b424f09b57b0ca7d4de5f1f7a
Diana Balderas <39641507+balderasdiana@users.noreply.github.com>
2021-10-21 23:00:40 -0700

    login / sign up
    works and information is stored in firebase database

---
+++ src/config/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyCmxQvFn8Kn3BFdqk_0vPx7vwwSQSGPfRM",
+  authDomain: "stickynotes-c9717.firebaseapp.com",
+  projectId: "stickynotes-c9717",
+  storageBucket: "stickynotes-c9717.appspot.com",
+  messagingSenderId: "85420865662",
+  appId: "1:85420865662:web:bc5e9d6c18d16af4fdc02f"
+};
+
+const fire = firebase.initializeApp(config);
+export default fire;
