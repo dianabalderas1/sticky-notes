b2dc765f495d6e7e13317509e738c4c42abbc862
DiegoLeon3 <leondiego101@gmail.com>
2021-11-10 10:59:12 -0800

    Can now push and retrieve data from database

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import fire from './config/fire';
 import StickyNote from './components/StickyNote';
 import Draggable from 'react-draggable';
 
 //accessing realTime DB
-const db = fire.database();
+let db = fire.database();
+type Note = { t: string; x: number; y: number; c: number };
+type Notes = { [key: string]: Note };
 
  //creating the reference to the collection 
 const notesRef = db.ref("Sticky");
-const  newNote = notesRef.push();
+// const  newNote = notesRef.push();
 
 class Home extends React.Component {
    
- 
-   
-
+  
   state = {
-    showSticky: false
+    stickyList: []
   }
+  
 
   logout() {
     fire.auth().signOut();
@@ -28,36 +29,52 @@ class Home extends React.Component {
     console.log('Success');
   }
 
-  createSticky = () => {
-    this.setState({showSticky: true});
-    console.log("Success");
-    return (<StickyNote />);
-  }
+  //add function will push to RealTime DB
+   add = () => {
+    const newPostKey = notesRef?.push().key ;
+    notesRef?.update({
+      [newPostKey]: {
+        t: "First Note",
+        x: window.scrollX + Math.floor(Math.random() * (200 - 80) + 80),
+        y: window.scrollY + Math.floor(Math.random() * (200 - 80) + 80),
+        c: 5,
+      },
+    });
+  };
 
-  onSubmit = () => {
-    this.setState({showSticky: true});
-    console.log("Success");
-    return (<StickyNote />);
+  //retrieve data from database 
+  retrieveData(){
+  notesRef.on('value', (snapshot) => {
+    let stickyList = [];
+    snapshot.forEach((data) => {
+      stickyList.push(data.val());
+    });
+    console.log(stickyList); 
+  }, (errorObject) => {
+    console.log('The read failed: ' + errorObject.name);
+  }); 
   }
-  
 
   render() {
     return (
-      <div 
-       >
+      <><div>
         {/* <h1>You are Logged In</h1> */}
         <Header />
-        <button onClick = {this.state.showSticky ? null : this.createSticky}>createStickyNote</button>
-        <button onClick = {this.logout}>Logout</button>
-        <Draggable>
+        <button onClick={() => this.add()}>createStickyNote</button>
+        <button onClick={() => this.retrieveData()}>Retrieve</button>
+        <button onClick={this.logout}>Logout</button>
+      </div>
+      <div>
+          <Draggable>
             <div>
               <textarea>
-              Edit text and move me around!
+                Edit text and move me around!
               </textarea>
-              <button>Submit</button>
             </div>
-      </Draggable> 
-      </div>
+          </Draggable>
+        </div></>
+         
+      
     )
   }
 }
