f181338ddc71b452aa3fac27a4b2866634afca8c
Carolina Martinez <78441764+cmartinez136@users.noreply.github.com>
2021-12-06 19:54:38 -0800

    commented updated function, not working

--- src/components/StickyNote.js
+++ src/components/StickyNote.js
@@ -28,7 +28,8 @@ const notesRef = db.ref("Sticky");
     const newPostKey = fire.database().ref("Sticky").push().key;
     db.ref("Sticky/").update({
       [newPostKey]: {
-        t: "text here",
+        t: "Edit text here",
+        pkey: newPostKey,
         x: window.scrollX + Math.floor(Math.random() * (200 - 80) + 80),
         y: window.scrollY + Math.floor(Math.random() * (200 - 80) + 80),
         c: fire.auth().currentUser.uid,
@@ -37,8 +38,9 @@ const notesRef = db.ref("Sticky");
   };
 
  const removeSticky = (key) => {
-    db.child(key).remove(); 
-  };
+    fire.database().ref().child(key).remove();
+
+};
 
  /*const add = () => {
     fire.database().ref("Sticky/").push({
@@ -48,18 +50,18 @@ const notesRef = db.ref("Sticky");
         userID: fire.auth().currentUser.uid
 })
 };*/
-
-// eslint-disable-next-line no-unused-vars
-/*export const removeSticky = () => {
-    fire.database().ref("Sticky/").remove({
-        text: "Note Text",
-        x: window.scrollX + Math.floor(Math.random() * (200 - 80) + 80),
-        y: window.scrollY + Math.floor(Math.random() * (200 - 80) + 80),
-        userID: fire.auth().currentUser.uid
-})
+/*const update = () => {
+    db.ref("Sticky/").update({
+        [newPostKey]: {
+          t: "Edit text here",
+          pkey: newPostKey,
+          x: window.scrollX + Math.floor(Math.random() * (200 - 80) + 80),
+          y: window.scrollY + Math.floor(Math.random() * (200 - 80) + 80),
+          c: fire.auth().currentUser.uid,
+        },
+      });
+    };
 };*/
-
-//  const update = (key, item) => db?.update({ [key]: item });
 
 class StickyNote extends React.Component {
    
@@ -94,6 +96,7 @@ class StickyNote extends React.Component {
           }}
         >
             <button class = "delete" onClick={removeSticky}>x</button>
+            <button class = "delete">✓</button>
             <textarea>
             {note.t}
             </textarea>
